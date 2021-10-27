7c59e5c3f10c36c7e1809ad42cc9170ca7046732
nk7822188 <nk7822188@gmail.com>
2021-10-27 09:06:25 +0530

    first react assign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
+export class ClassComponent extends Component{
+  render(){
+    return(
+      <div className="pq">
+        <h1>This is created using Functional Based Components</h1>
+        <p>This is done using external css</p>
+        <p style={{color:"blue"}}>This is done using inline css </p>
+      </div>
+    );
+  }
+
+}
+ export function FunctionComponent() {
+   return(
+    <div className="qp">
+      <h1>This is created using Functional Based Components</h1>
+      <p>This is done using external css</p>
+      <p style={{color:"blue"}}>This is done using inline css</p>
+    </div>
+   );
+ }
+ 
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="heading" >Styling using Functional and Class Component </h1>
+      <button className="sm"> this is functional Component</button>
+      <button className="ms">this is class Component</button>
+     
+    </div> 
   );
 }
 
 export default App;
+
